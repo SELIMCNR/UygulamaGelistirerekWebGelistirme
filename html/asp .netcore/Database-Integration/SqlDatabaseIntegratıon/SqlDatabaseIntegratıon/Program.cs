4ca71ec52fb333b5ac64e95650fc40aa3ccb1418
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-04-09 15:39:43 +0300

    Add files via upload

---
+++ html/asp .netcore/Database-Integration/SqlDatabaseIntegratıon/SqlDatabaseIntegratıon/Program.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        GetSqlConnection();
+        GetMYSqlConnection();
+    }
+
+    static void GetSqlConnection()
+    {
+        string connectionString = @"Data Source=DESKTOP-BC3LOP2\SELM;Initial Catalog=one;Integrated Security=SSPI;";
+    
+        
+        using(var connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Bağlantı sağlandı");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+
+    static void GetMYSqlConnection()
+    {
+        string connectionString = @"server=localhost;port=3306;database=db_prd;user=root;password=419293A23.m";
+
+
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Bağlantı sağlandı mysql");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
