01b8362e0f92067d3d2c58127af79f7ea247c67b
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-04-16 23:47:56 +0300

    Add files via upload

--- html/asp .netcore/Database-Integration/SqlDatabaseIntegratıon/SqlDatabaseIntegratıon/Program.cs
+++ html/asp .netcore/Database-Integration/SqlDatabaseIntegratıon/SqlDatabaseIntegratıon/Program.cs
@@ -1,13 +1,74 @@
 ﻿using MySql.Data.MySqlClient;
+using SqlDatabaseIntegratıon;
 using System.Data.SqlClient;
 
+
+interface IProductDal
+{
+    List<Product> GetAllProducts();    
+    Product GetProductById(int id);
+
+    void Create(Product p);
+    void Update(Product p);
+    void Delete(int productId);
+}
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        GetSqlConnection();
-        GetMYSqlConnection();
+       var products= GetAllProducts();
+
+        foreach (var product in products)
+        {
+            Console.WriteLine($"name {product.Name} price : {product.Price}");
+        }
+    }
+
+    static List<Product> GetAllProducts()
+    {
+        List<Product> products = null;
+        using (var connection = GetMYSqlConnection())
+        {
+            try
+            {
+                connection.Open();
+                string sql = "Select * from product";
+              //  SqlCommand command = new SqlCommand(sql,connection);
+                
+                MySqlCommand command = new MySqlCommand(sql, connection);   
+                
+
+                MySqlDataReader reader = command.ExecuteReader();
+                
+
+             products  = new List<Product>();
+
+
+                while (reader.Read())
+                {
+                    products.Add(new Product
+                    {
+                        ProductId = int.Parse(reader[1].ToString()),
+                        Name = reader["name"].ToString(),
+                        Price = double.Parse(reader["price"].ToString()),
+
+                    }) ;
+
+            
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        return products;
     }
 
     static void GetSqlConnection()
@@ -34,27 +95,15 @@ internal class Program
 
     }
 
-    static void GetMYSqlConnection()
+    static MySqlConnection  GetMYSqlConnection()
     {
         string connectionString = @"server=localhost;port=3306;database=db_prd;user=root;password=419293A23.m";
 
 
-        using (var connection = new MySqlConnection(connectionString))
-        {
-            try
-            {
-                connection.Open();
-                Console.WriteLine("Bağlantı sağlandı mysql");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                connection.Close();
-            }
+        return new MySqlConnection(connectionString);
+       
+         
         }
 
     }
-}
+
